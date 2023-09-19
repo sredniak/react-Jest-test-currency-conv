67b2b1e691fcd07f5945282b3a66bdc87637095c
Adrian Średniawski <sredni98123@gmail.com>
2023-09-19 08:59:54 +0200

    Test component CurrencyForm

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,11 +1,19 @@
 export const convertPLNToUSD = (PLN) => {
 
   const PLNtoUSD = PLN / 3.5;
-  
+
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD'
   });
 
-  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+  if (typeof PLN === 'string' || typeof PLN === 'undefined') {
+    return NaN;
+  } else if (PLN < 0) {
+    return '$0.00';
+  } else if (typeof PLN !== 'string' && typeof PLN !== 'number') {
+    return 'Error';
+  } else {
+    return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+  }
 }
